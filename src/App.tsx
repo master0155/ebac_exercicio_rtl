fb246516a4e9bdc97a78d6ac2d3448e546da32b3
Gabriel <gabrielsilvalima1994@gmail.com>
2026-02-15 20:27:36 -0300

    suas alterações

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://cdn.awsli.com.br/2500x2500/2571/2571273/produto/210237528/batmovel-the-batman-2022-figura-batman-jada-toys-118-com-luzes-50504cf8.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,21 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+import modelComments from '../../models/Comment'
+
+function addComment(comment: modelComments){
+    fireEvent.change(screen.getByTestId('textarea'),{
+        target: {
+            value: comment.comment
+        }
+    });
+    fireEvent.click(screen.getByTestId('btn-add-comment'))
+}
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        addComment({ id: 1, comment: 'teste 1'})
+        addComment({ id: 2, comment: 'teste 2'})
+        expect(screen.getByText(/teste 2/i)).toBeInTheDocument()
     });
 });
